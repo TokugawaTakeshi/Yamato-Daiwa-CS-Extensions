c1d1e65f33d46a40d3599227e7758a50f0cdf7aa
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-09-03 13:34:16 +0900

    Initialize repository

---
+++ DateOnlyExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace YamatoDaiwaCS_Extensions;
+
+
+public static class DateOnlyExtensions
+{
+  
+  public static DateOnly CreateDateOnlyFromISO8601_String(string ISO8601_String) {
+    DateTime dateTime = DateTime.Parse(ISO8601_String, null, System.Globalization.DateTimeStyles.RoundtripKind);
+    return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+  }
+
+  public static string ToISO8601_String(this DateOnly self) {
+    return self.ToString("yyyy-MM-dd");
+  }
+  
+}
