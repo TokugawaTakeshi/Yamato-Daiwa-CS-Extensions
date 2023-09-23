6d648b2bd0a36dc4c35935e1475c883832dd9326
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-09-23 15:26:11 +0900

    0.0.1

---
+++ DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace YamatoDaiwaCS_Extensions;
+
+
+public static class DateTimeExtensions
+{
+  
+  public static DateTime createDateTimeFromISO8601_String(string ISO8601_String) {
+    return DateTime.Parse(ISO8601_String, null, System.Globalization.DateTimeStyles.RoundtripKind);
+  }
+
+  public static string ToISO8601_String(this DateTime self) {
+    return self.ToString( "yyyy-MM-dd'T'HH:mm:ss'Z'");
+  }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# Yamato Daiwa CS(harp) extensions
+
+## Extensions of `DateOnly` class
+
+* `DateOnly CreateDateOnlyFromISO8601_String(string ISO8601_String)`
+* `string ToISO8601_String(this DateOnly self)`
+
+
+## Extensions of `DateTime` class
+
+* `DateOnly CreateDateOnlyFromISO8601_String(string ISO8601_String)`
+* `string ToISO8601_String(this DateOnly self)`
