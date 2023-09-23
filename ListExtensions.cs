070ddda140aec15ec46c4ac09f471b0c24154dcb
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-09-23 15:47:16 +0900

    0.0.2

---
+++ ListExtensions.cs
@@ -0,0 +1,121 @@
+﻿namespace YamatoDaiwaCS_Extensions;
+
+
+public static class ListExtensions
+{
+
+  public static List<TElement> AddElementsToEnd<TElement> (
+    this List<TElement> self,
+    params TElement[] newElements
+  ) {
+
+    foreach (TElement element in newElements)
+    {
+      self.Add(element);   
+    }
+
+    return self;
+
+  }
+  
+  
+  public static List<TElement> AddElementToEndIf<TElement>(
+    this List<TElement> self,
+    TElement newElement,
+    bool condition
+  )
+  {
+
+    if (condition)
+    {
+      self.Add(newElement);
+    }
+
+    return self;
+
+  }
+  
+  public static List<TElement> AddElementToEndIf<TElement>(
+    this List<TElement> self,
+    TElement newElement,
+    Func<TElement?, bool> condition
+  )
+  {
+
+    if (condition(newElement))
+    {
+      self.Add(newElement);
+    }
+
+    return self;
+
+  }
+  
+
+  public static string StringifyEachElementAndJoin<TElement>(
+    this List<TElement> self,
+    string separator
+  )
+  {
+    return String.Join(separator, self);
+  }
+
+
+  public static List<TElement> ReplaceArrayElementsByPredicate<TElement>(
+    this List<TElement> self,
+    Func<TElement, bool> predicate,
+    TElement newElement,
+    bool mustReplaceOnlyFirstOne
+  )
+  {
+      
+    foreach ((TElement element, int index) in self.Select((element, index) => (element, index)))
+    {
+
+      if (!predicate(element))
+      {
+        continue;
+      }
+      
+      
+      self[index] = newElement;
+          
+      if (mustReplaceOnlyFirstOne) {
+        return self;
+      }
+      
+    }
+      
+    return self;
+      
+  }
+
+  public static List<TElement> ReplaceArrayElementsByPredicate<TElement>(
+    this List<TElement> self,
+    Func<TElement, bool> predicate,
+    Func<TElement, TElement> replacer,
+    bool mustReplaceOnlyFirstOne
+  )
+  {
+      
+    foreach ((TElement element, int index) in self.Select((element, index) => (element, index)))
+    {
+      if (!predicate(element))
+      {
+        continue;
+      }
+      
+      
+      self[index] = replacer(element);
+          
+      if (mustReplaceOnlyFirstOne) {
+        return self;
+      }
+      
+    }
+      
+    return self;
+      
+  }
+  
+}
--- README.md
+++ README.md
@@ -3,10 +3,22 @@
 ## Extensions of `DateOnly` class
 
 * `DateOnly CreateDateOnlyFromISO8601_String(string ISO8601_String)`
-* `string ToISO8601_String(this DateOnly self)`
+* `string ToISO8601_String()`
 
 
 ## Extensions of `DateTime` class
 
 * `DateOnly CreateDateOnlyFromISO8601_String(string ISO8601_String)`
-* `string ToISO8601_String(this DateOnly self)`
+* `string ToISO8601_String()`
+
+
+## Extensions of `List` class
+
+* `List<TElement> AddElementsToEnd<TElement> (params TElement[] newElements) `
+* `AddElementToEndIf`
+  * `List<TElement> AddElementToEndIf<TElement>(TElement newElement, bool condition)`
+  * `List<TElement> AddElementToEndIf<TElement>(TElement newElement, Func<TElement?, bool> condition)`
+* `StringifyEachElementAndJoin<TElement>(string separator)` 
+* `ReplaceArrayElementsByPredicate`
+  * `List<TElement> ReplaceArrayElementsByPredicate<TElement>(Func<TElement, bool> predicate, TElement newElement, bool mustReplaceOnlyFirstOne)`
+  * `List<TElement> ReplaceArrayElementsByPredicate<TElement>(Func<TElement, bool> predicate, Func<TElement, TElement> replacer, bool mustReplaceOnlyFirstOne)`
